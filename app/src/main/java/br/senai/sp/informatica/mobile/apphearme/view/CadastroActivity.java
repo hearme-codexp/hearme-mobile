8bdf5d3ed975ec08963beb78c3ad746a3316e8bf
vivi <viviteodoro19@gmail.com>
2018-03-02 18:57:21 -0300

    add HomeActivity e CadastroActivity

---
+++ app/src/main/java/br/senai/sp/informatica/mobile/apphearme/view/CadastroActivity.java
@@ -0,0 +1,15 @@
+package br.senai.sp.informatica.mobile.apphearme.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.senai.sp.informatica.mobile.apphearme.R;
+
+public class CadastroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro);
+    }
+
+}
