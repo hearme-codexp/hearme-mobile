ec6a422cf17e6f2ce3fbcc1651b0256a0a01a27b
vivi <viviteodoro19@gmail.com>
2018-03-02 20:34:43 -0300

    navegacao entre as telas

--- app/src/main/java/br/senai/sp/informatica/mobile/apphearme/view/MainActivity.java
+++ app/src/main/java/br/senai/sp/informatica/mobile/apphearme/view/MainActivity.java
@@ -1,15 +1,35 @@
 package br.senai.sp.informatica.mobile.apphearme.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import br.senai.sp.informatica.mobile.apphearme.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etLogin;
+    private EditText etSenha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etLogin = findViewById(R.id.etLogin);
+        etSenha = findViewById(R.id.etSenha);
+    }
+
+    public void btnEntrar(View view){
+        String login = etLogin.getText().toString();
+        String senha = etSenha.getText().toString();
+
+        startActivity(new Intent(this, HomeActivity.class));
+    }
+
+    public void btnCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
