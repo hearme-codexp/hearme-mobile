51337e17edee7346a2a535b217cd76353a29bb15
vivi <viviteodoro19@gmail.com>
2018-03-15 21:37:03 -0300

    metodo scan atualizado

--- app/src/main/java/br/senai/sp/informatica/mobile/apphearme/view/BlueActivity.java
+++ app/src/main/java/br/senai/sp/informatica/mobile/apphearme/view/BlueActivity.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -22,13 +23,16 @@ import java.util.ArrayList;
 import java.util.Set;
 
 import br.senai.sp.informatica.mobile.apphearme.R;
+import br.senai.sp.informatica.mobile.apphearme.model.Device;
 
 public class BlueActivity extends AppCompatActivity {
 
     private Button swOnOff, btnlist, btnVisible;
     private BluetoothAdapter bluetoothAdapter;
-    private Set<BluetoothDevice>pairedDevices;
+    private Set<BluetoothDevice> pairedDevices;
     ListView listView;
+    ArrayList list = new ArrayList();
+    protected static final int DISCOVERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,57 +47,93 @@ public class BlueActivity extends AppCompatActivity {
         listView = findViewById(R.id.blueList);
     }
 
-    public void setSwOnOff(View view){
-        Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        startActivityForResult(turnOn, 0);
-        Toast.makeText(getApplicationContext(), "Blue on", Toast.LENGTH_SHORT).show();
+    public void setSwOnOff(View view) {
+        //TODO: Desligar bluetooth
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Blue on", Toast.LENGTH_SHORT).show();
+        }
     }
 
+    /*
     public void visible(View view){
         Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
         startActivityForResult(getVisible, 0);
+    }*/
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            list.add(device.getName() + "\n" + device.getAddress());
+        }
+    };
+
+    public void list(View view) {
+        //pairedDevices = bluetoothAdapter.getBondedDevices();
+        String scanModeChanged = BluetoothAdapter.ACTION_SCAN_MODE_CHANGED;
+        String beDiscoverable = BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE;
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(broadcastReceiver, filter);
+        startActivityForResult(new Intent(beDiscoverable), DISCOVERY_REQUEST);
     }
 
-    public void list(View view){
-        pairedDevices = bluetoothAdapter.getBondedDevices();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == DISCOVERY_REQUEST) {
+            Toast.makeText(this, "Discovery in progress", Toast.LENGTH_SHORT).show();
+            findDevices();
+        }
+    }
+
+    private void findDevices() {
+        /*pairedDevices = bluetoothAdapter.getBondedDevices();
         ArrayList list = new ArrayList();
 
         for(BluetoothDevice btDev : pairedDevices) list.add(btDev.getName());
         Toast.makeText(getApplicationContext(), "Lista dev pareados", Toast.LENGTH_SHORT).show();
 
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
+        listView.setAdapter(adapter);*/
+
+        String lastUsedRemoteDevice = getLastUsedRemoteBTDevice();
+        if(lastUsedRemoteDevice != null){
+            String toastText = "Checking for known paired devices, namely: " + lastUsedRemoteDevice;
+            Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+            pairedDevices = bluetoothAdapter.getBondedDevices();
+
+            for(BluetoothDevice btDev : pairedDevices){
+                if(btDev.getAddress().equals(lastUsedRemoteDevice)){
+                    Toast.makeText(getApplicationContext(), "Lista dev pareados", Toast.LENGTH_SHORT).show();
+                    list.add(btDev.getName());
+                }
+            }
+        }
+
+        if(list == null){
+            Toast.makeText(this, "starting discovery for remote devices", Toast.LENGTH_SHORT).show();
+                if(bluetoothAdapter.startDiscovery()){
+                    Toast.makeText(this, "Scanning for devices", Toast.LENGTH_SHORT).show();
+                    registerReceiver(discovery_result, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+                }
+        }
+
     }
 
-    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+    BroadcastReceiver discovery_result = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+            String action = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                ArrayList list = new ArrayList();
-                list.add(device.getName());
-                final ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),
-                        android.R.layout.simple_list_item_1, list);
-                listView.setAdapter(adapter);
-            }
+                list.add(device.getName() + "\n" + device.getAddress());
         }
     };
 
-    public void metScan(LayoutInflater inflater, ViewGroup container, View view){
-        view = inflater.inflate(R.layout.bluetooth, container, false);
-        ToggleButton scan = view.findViewById(R.id.tgScan);
-        scan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                if(isChecked){
-                    bluetoothAdapter.startDiscovery();
-                }else{
-                    bluetoothAdapter.cancelDiscovery();
-                }
-            }
-        });
+    private String getLastUsedRemoteBTDevice(){
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        String result = preferences.getString("LAST_REMOTE_DEVICE_ADDRESS", null);
+        return result;
     }
-
 }
