41e3f4de39337ab319e02024a126c350a3f73991
vivi <viviteodoro19@gmail.com>
2018-04-06 22:11:17 -0300

    Juntar activities de bluetoot

--- app/src/main/java/br/senai/sp/informatica/mobile/apphearme/model/MyLocationListener.java
+++ app/src/main/java/br/senai/sp/informatica/mobile/apphearme/model/MyLocationListener.java
@@ -1,51 +1,33 @@
 package br.senai.sp.informatica.mobile.apphearme.model;
 
 import android.content.Context;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 import static android.content.ContentValues.TAG;
 
 public class MyLocationListener implements LocationListener{
     Context ctx;
+    Location loc;
+
+    public MyLocationListener(Location initialLocation) {
+        this.loc = initialLocation;
+    }
 
     @Override
     public void onLocationChanged(Location loc){
-        //editLocation.setText("");
-        //pb.setVisibility(View.INVISIBLE);
+        this.loc = loc;
         Toast.makeText(ctx, "Location changed: Lat: " + loc.getLatitude() +
         " Lng: " +  loc.getLongitude(), Toast.LENGTH_SHORT).show();
         String longitude = "Longitude: " + loc.getLongitude();
         Log.v(TAG, longitude);
         String latitude = "Latitude: " + loc.getLongitude();
         Log.v(TAG, latitude);
-
-        String cityName = null;
-        Geocoder gcd = new Geocoder(ctx, Locale.getDefault());
-        List<Address> addresses;
-        try{
-            addresses = gcd.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
-            if(addresses.size() > 0){
-                String msg = addresses.get(0).getLocality();
-                Toast.makeText(ctx, msg , Toast.LENGTH_SHORT).show();
-                cityName = addresses.get(0).getLocality();
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        String s = longitude + "\n" + latitude + "\nMy current city is: " + cityName;
-        Toast.makeText(ctx, s , Toast.LENGTH_SHORT).show();
-        //editLocation.setText(s);
     }
     @Override
     public void onProviderDisabled(String provider){}
@@ -56,4 +38,7 @@ public class MyLocationListener implements LocationListener{
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras){}
 
+    public Location getLoc() {
+        return loc;
+    }
 }
